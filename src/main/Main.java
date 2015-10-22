444674d68f6c3c7cabf40886cbe919982564f4de
Kerkox <bryanpoolker@gmail.com>
2015-10-22 02:59:28 -0500

    Version 0.0.99 GUI Resources +Splahs Image +Carga

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,16 @@
 package main;
 
 import Matricula.UI.Principal;
+import Matricula.UI.load.PantallaCargandoMain;
 import Matricula.logic.Periodo;
 import Matricula.logic.*;
 import Matricula.logic.enumclass.Jornada;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.SplashScreen;
+import java.awt.geom.Rectangle2D;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -18,12 +25,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        splashInit();           // initialize splash overlay drawing parameters
+        appInit();              // simulate what an application would do 
 
+        // before starting
+        if (mySplash != null) // check if we really had a spash screen
+        {
+            mySplash.close();   // if so we're now done with it
+        }
+        // begin with the interactive portion of the program
         try {
 
             Periodo periodo = new Periodo("Agosto", "Diciembre", 2015);
             Universidad u = new Universidad("800", "Univalle", "Carbonera");
             u.setPeriodoActual(periodo);
+            
 
             //Estudiantes            
             Estudiante[] students = {
@@ -93,16 +109,16 @@ public class Main {
                 new Curso((byte) 50, cupos[0], teachers[0], subjects[0]),
                 new Curso((byte) 51, cupos[1], teachers[1], subjects[2])
             };
-
-            if (u.getPeridoActual().getCursos().isEmpty()) {
-                System.out.println("Aqui no hay Cursos todavia");
-
-                //Registro de Cursos
-                for (Curso x : cursos) {
-                    u.registrar(x);
-                }
-
-            }
+//################################################################
+//            if (u.getPeridoActual().getCursos().isEmpty()) {
+//                System.out.println("Aqui no hay Cursos todavia");
+//
+//                //Registro de Cursos
+//                for (Curso x : cursos) {
+//                    u.registrar(x);
+//                }
+//                System.out.println("Termino de hacer los registros");
+//            }
             if (u.getPeridoActual() == null) {
                 System.out.println("Entro aqui porque no tiene perido actual");
 
@@ -110,11 +126,118 @@ public class Main {
 
 //*********************************
             System.out.println("Debe de hacer la conexion a la bd");
+            //################################################################
+//            new PantallaCargandoMain();
             new Principal(u).setVisible(true);
 
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, ex.getMessage());
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Prepare the global variables for the other splash functions
+     */
+    static Rectangle2D.Double splashTextArea;
+    static Rectangle2D.Double splashProgressArea;
+    static Graphics2D splashGraphics;
+    static Font font;
+    static SplashScreen mySplash;
+
+    private static void splashInit() {
+
+        mySplash = SplashScreen.getSplashScreen();
+        if (mySplash != null) {   // if there are any problems displaying the splash this will be null
+            Dimension ssDim = mySplash.getSize();
+            int height = ssDim.height;
+            int width = ssDim.width;
+            // stake out some area for our status information
+            splashTextArea = new Rectangle2D.Double(15., height * 0.88, width * .45, 32.);
+            splashProgressArea = new Rectangle2D.Double(width * .55, height * .92, width * .4, 12);
+
+            // create the Graphics environment for drawing status info
+            splashGraphics = mySplash.createGraphics();
+            font = new Font("Dialog", Font.PLAIN, 18);
+            splashGraphics.setFont(font);
+
+            // initialize the status info
+            splashText("Starting");
+            splashProgress(0);
+        }
+    }
+
+    /**
+     * Display text in status area of Splash. Note: no validation it will fit.
+     *
+     * @param str - text to be displayed
+     */
+    public static void splashText(String str) {
+        if (mySplash != null && mySplash.isVisible()) {   // important to check here so no other methods need to know if there
+            // really is a Splash being displayed
+
+            // erase the last status text
+            splashGraphics.setPaint(Color.LIGHT_GRAY);
+            splashGraphics.fill(splashTextArea);
+
+            // draw the text
+            splashGraphics.setPaint(Color.BLACK);
+            splashGraphics.drawString(str, (int) (splashTextArea.getX() + 10), (int) (splashTextArea.getY() + 15));
+
+            // make sure it's displayed
+            mySplash.update();
+        }
+    }
+
+    /**
+     * Display a (very) basic progress bar
+     *
+     * @param pct how much of the progress bar to display 0-100
+     */
+    public static void splashProgress(int pct) {
+        if (mySplash != null && mySplash.isVisible()) {
+
+            // Note: 3 colors are used here to demonstrate steps
+            // erase the old one
+            splashGraphics.setPaint(Color.LIGHT_GRAY);
+            splashGraphics.fill(splashProgressArea);
+
+            // draw an outline
+            splashGraphics.setPaint(Color.BLUE);
+            splashGraphics.draw(splashProgressArea);
+
+            // Calculate the width corresponding to the correct percentage
+            int x = (int) splashProgressArea.getMinX();
+            int y = (int) splashProgressArea.getMinY();
+            int wid = (int) splashProgressArea.getWidth();
+            int hgt = (int) splashProgressArea.getHeight();
+
+            int doneWidth = Math.round(pct * wid / 100.f);
+            doneWidth = Math.max(0, Math.min(doneWidth, wid - 1));  // limit 0-width
+
+            // fill the done part one pixel smaller than the outline
+            splashGraphics.setPaint(Color.GREEN);
+            splashGraphics.fillRect(x, y + 1, doneWidth, hgt - 1);
+
+            // make sure it's displayed
+            mySplash.update();
+        }
+    }
+
+    /**
+     * just a stub to simulate a long initialization task that updates the text
+     * and progress parts of the status in the Splash
+     */
+    private static void appInit() {
+        for (int i = 1; i <= 10; i++) {
+            int pctDone = i * 10;
+            splashText("Cargando... " + i*10+"%");
+            splashProgress(pctDone);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ex) {
+                // ignore it
+            }
         }
     }
 
